c68387b3decd79cd916bfd7d177b4da13108003a
sahil <dshiv867@gmail.com>
2023-08-24 01:27:52 +0530

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { signOut, useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
   const { data: user } = useCurrentUser();
@@ -14,14 +15,7 @@ export default function Home() {
   });
   return (
     <>
-      <h1 className="text-4xl text-rose-300">Hello world</h1>
-      <p className="text-2xl text-emerald-300">{user?.name}</p>
-      <button
-        className="bg-red-500 rounded px-5 py-2"
-        onClick={() => signOut()}
-      >
-        Sign out
-      </button>
+      <Navbar />
     </>
   );
 }
